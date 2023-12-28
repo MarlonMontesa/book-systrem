09bbac7024955c4ca267d5c8fb7248375ff7f3a3
MarlonMontesa <112678625+MarlonMontesa@users.noreply.github.com>
2023-12-28 21:45:21 +0800

    Delete config.js
    cannot be disclosed

--- config.js
+++
@@ -1,8 +0,0 @@
-export const PORT = 5555; // this is the port
-export const mongoDbURL = 
-'mongodb+srv://montesamarlon20:Pulburon200@cluster0.pcnid.mongodb.net/' // this the connection string that you can get on mongodb
-
-// 'mongodb+srv://montesamarlon20:root@cluster0.pcnid.mongodb.net/?retryWrites=true&w=majority'
-//mongodb+srv://montesamarlon20:<password>@cluster0.pcnid.mongodb.net/?retryWrites=true&w=majority
-//mongodb+srv://montesamarlon20:Pulburon200@cluster0.pcnid.mongodb.ne/
-//mongodb+srv://montesamarlon20:<password>@cluster0.pcnid.mongodb.n
